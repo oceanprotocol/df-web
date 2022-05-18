d80bdbf553debb0d6bcc180b483ddad0cd71f721
Norbert <katunanorbert@gmail.com>
2022-05-18 11:43:08 +0300

    created component for displaying messages

---
+++ src/components/common/MainMessage.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let title = undefined;
+  export let message = undefined;
+</script>
+
+<div class="container">
+  <span>{title}</span>
+  <p>{message}</p>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: calc(var(--spacer) / 4);
+  }
+  span {
+    font-weight: bold;
+  }
+  p {
+    font-size: var(--font-size-normal);
+  }
+</style>
