4bcfdc2293ee582af3e2d73407121b895bbf8b17
idiom-bytes <69865342+idiom-bytes@users.noreply.github.com>
2022-05-25 15:38:03 -0700

    Adjusting indenting and console logs.

--- src/utils/cli/dispense-docker.js
+++ src/utils/cli/dispense-docker.js
@@ -14,25 +14,21 @@ async function dispense(cmd) {
             resolve();
         })
     })
-        .then(() => {
-            console.log("Success!")
-        })
-        .catch((err) => {
-            console.log("Exception!")
-        })
+    .then(() => {
+        console.log("Success!")
+    })
+    .catch((err) => {
+        console.log("Exception!")
+    })
 }
 
 async function dispenseAllRewards() {
-    console.log(">>>>> Rewards #1");
+    console.log(">>>>> Rewards Rinkeby");
     await dispense('./dfpy_docker dispense /app/data/rewards/ 4 0x4751774A124D02f1611dFe17f4d697dDdF932Fd5 0xe6239d757c064c237dF31e08EbdD582f0608aCE0 >> out.log')
-
-    console.log(">>>>> Rewards #2");
     await dispense('./dfpy_docker dispense /app/data/rewards/ 4 0x4751774A124D02f1611dFe17f4d697dDdF932Fd5 0xc6913d3eCed79021a39E6955015313B22B72b76E >> out.log');
 
-    console.log(">>>>> Rewards #3");
+    console.log(">>>>> Rewards Ropsten");
     await dispense('./dfpy_docker dispense /app/data/rewards/ 3 0x8FD70a9E20DAcDff6ab5905E94742afE5AE40f16 0x400a17C7644fEF90EC5e85C59BA2034A6D4B1366 >> out.log');
-
-    console.log(">>>>> Rewards #4");
     await dispense('./dfpy_docker dispense /app/data/rewards/ 3 0x8FD70a9E20DAcDff6ab5905E94742afE5AE40f16 0x0d92cadB0A0BC3693e985FB15E47BcF4d1Dc3792 >> out.log');
 }
 
