a5065aef12b9e642e4e87138e16821c0040c923c
idiom-bytes <69865342+idiom-bytes@users.noreply.github.com>
2022-06-08 21:21:02 -0700

    Disabling chains for now.

--- src/app.config.js
+++ src/app.config.js
@@ -1,8 +1,8 @@
 import networks from "./networks-metadata.json"
 
 //for production
-export const supportedChainIds = [1, 137, 56, 246, 1285];
-export const initChainIds = [1, 137, 56, 246, 1285];
+export const supportedChainIds = []; //[1, 137, 56, 246, 1285];
+export const initChainIds = []; //[1, 137, 56, 246, 1285];
 
 export const getNetworkByChainId = async (chainId) => {
     if (!chainId) return false;
