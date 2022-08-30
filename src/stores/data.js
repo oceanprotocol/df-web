28a66616f9917153315c60ebd200560c921d2cb3
Norbert <katunanorbert@gmail.com>
2022-08-30 15:23:38 +0300

    enable view nft action

--- src/stores/data.js
+++ src/stores/data.js
@@ -63,21 +63,37 @@ function getRow(dataInfo, key) {
     basetoken: getTokenSymbolByAddress(dataInfo.basetoken),
     basetokenaddress: '0x2473f4F7bf40ed9310838edFCA6262C17A59DF64'.toLocaleLowerCase(),
     nftaddress: dataInfo.nft_addr,
-    volume: parseFloat(dataInfo.vol_amt).toFixed(3),
+    volume: parseFloat(dataInfo.volume).toFixed(3),
     allocate: {
       chainId: 8996,
-      url: dataInfo.url,
       nftAddress: dataInfo.nft_addr,
       basetokenAddress: '0x2473f4F7bf40ed9310838edFCA6262C17A59DF64'.toLocaleLowerCase(),
       basetoken: getTokenSymbolByAddress(dataInfo.basetoken),
-      volume: parseFloat(dataInfo.vol_amt),
+      volume: parseFloat(dataInfo.volume),
     },
-    action: dataInfo.url,
+    action: `https://market.oceanprotocol.com/asset/${dataInfo.did}`,
   };
 }
 
 export async function loadDatasets(nftsApi, allocations) {
-  const allDatasets = await getDatasets(nftsApi);
+  //const allDatasets = await getDatasets(nftsApi);
+  const allDatasets = [{basetoken_addr: "0x8967bcf84170c91b0d24d4302c2376283b0b3a07",
+  chainID: 4,
+  nft_addr: "0x537e625c1d722fef6a6e793ac226e5f22e485923",
+  basetoken_symbol: "OCEAN",
+  did: "did:op:aee900df7379cda6a5aa1b87bd77e053906002058f649825df0bffe5d8cf17dc",
+  volume: 11.103105545},
+  {basetoken_addr: "0x8967bcf84170c91b0d24d4302c2376283b0b3a07",
+  chainID: 4,
+  nft_addr: "0x537e625c1d722fef6a6e793ac226e5f22e485923",
+  basetoken_symbol: "OCEAN",
+  did: "did:op:aee900df7379cda6a5aa1b87bd77e053906002058f649825df0bffe5d8cf17dc",
+  volume: 11.103105545},{basetoken_addr: "0x8967bcf84170c91b0d24d4302c2376283b0b3a07",
+  chainID: 4,
+  nft_addr: "0x537e625c1d722fef6a6e793ac226e5f22e485923",
+  basetoken_symbol: "OCEAN",
+  did: "did:op:aee900df7379cda6a5aa1b87bd77e053906002058f649825df0bffe5d8cf17dc",
+  volume: 11.103105545}]
   console.log(allDatasets, allocations)
   if (allDatasets.length === 0) {
     datasets.set([]);
