d6763f3820a95d1db3731ebd6d352824ff8e2413
Norbert <katunanorbert@gmail.com>
2022-09-14 15:28:48 +0300

    fix get estimated DF rewards

--- src/components/common/EstimatedRewards.svelte
+++ src/components/common/EstimatedRewards.svelte
@@ -1,29 +1,21 @@
 <script>
   import { rewards } from "../../stores/airdrops";
-  import TokenReward from "../common/TokenReward.svelte";
   import ItemWithLabel from "./ItemWithLabel.svelte";
-  let estimatedRewards;
+  let estimatedVE = 0.0;
   let estimatedDF = 0;
 
   function getEstimatedRewards() {
-    estimatedRewards = {};
-    $rewards.forEach((reward) => {
-      if (!estimatedRewards[reward.token]) {
-        estimatedRewards[reward.token] = 0;
-      }
-      estimatedDF += reward.amt;
-      estimatedRewards[reward.token] += reward.amt;
-    });
+    estimatedDF = $rewards.map((reward) => reward.balance);
   }
 
-  $: if ($rewards && !estimatedRewards) {
+  $: if ($rewards) {
     getEstimatedRewards();
   }
 </script>
 
 <div class="container">
-  <ItemWithLabel value={estimatedDF} title="Total Estimated" />
-  <ItemWithLabel value={estimatedDF} title="Estimated Fee" />
+  <ItemWithLabel value={estimatedDF + estimatedVE} title="Total Estimated" />
+  <ItemWithLabel value={estimatedVE} title="Estimated Fee" />
   <ItemWithLabel value={estimatedDF} title="Estimated DF" />
 </div>
 
