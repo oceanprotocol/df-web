5e094fdaa0807c5ab499068838edef79bf213717
Idiom <69865342+idiom-bytes@users.noreply.github.com>
2022-09-29 15:26:46 -0700

    Revert "Revert "Implementing all checks and hooks for claiming rewards.""

--- src/utils/feeEstimate.js
+++ src/utils/feeEstimate.js
@@ -1,35 +1,16 @@
 import {ethers} from "ethers";
-import {get} from "svelte/store";
 import * as VeFeeEstimateABI from "./abis/veFeeEstimateABI";
-import { networkSigner } from "../stores/web3";
 
 const veFeeEstimateABI = VeFeeEstimateABI.default
 
-export const getRewardsFeeEstimate = async(userAddress) => {
+export const getRewardsFeeEstimate = async(userAddress, provider) => {
     try {
-      const contract = new ethers.Contract(process.env.VE_FEE_ESTIMATE_CONTRACT, veFeeEstimateABI, get(networkSigner));
+      const contract = new ethers.Contract(process.env.VE_FEE_ESTIMATE_CONTRACT, veFeeEstimateABI, provider);
       const estimateClaim = await contract.estimateClaim(userAddress)
       const estimateClaimFormatted = ethers.utils.formatEther(BigInt(estimateClaim).toString(10))
       return estimateClaimFormatted
   } catch (error) {
     console.log(error)
     throw error;
-  }
-}
-
-export async function claimVERewards(userAddress, signer) {
-  try {
-    // ABI function is overriden, specify which fn to use to avoid crashing
-    const contract = new ethers.Contract(
-          process.env.VE_FEE_DISTRIBUTOR_CONTRACT,
-          ["function claim(address _addr) returns (uint 256)"],
-          get(networkSigner)
-      );
-      const resp = await contract.claim(userAddress);
-      await resp.wait();
-      console.log("Success claiming rewards, txReceipt here", resp);
-  }catch (error) {
-      console.log("Error claiming rewards :", error);
-      throw error;
   }
 }
