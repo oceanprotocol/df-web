7e6cd6a834781782f788446dff396b3bfca67785
Norbert <katunanorbert@gmail.com>
2022-10-19 09:32:26 +0100

    format epoch start_date to be the consistent format trough app

--- src/components/claim/EpochHistory.svelte
+++ src/components/claim/EpochHistory.svelte
@@ -1,19 +1,19 @@
 <script>
   import { DataTable } from "carbon-components-svelte";
-  import * as epochs from "../../utils/metadata/epochs/epochs.json"; 
-  import {getEpoch} from "../../utils/epochs"
+  import * as epochs from "../../utils/metadata/epochs/epochs.json";
+  import { getEpoch } from "../../utils/epochs";
   import moment from "moment";
-  
+
   let loading = true;
   let curEpoch;
   let rows = [];
-  
+
   const headers = [
     { key: "id", value: "Round" },
     { key: "date_start", value: "Start Date" },
-    { key: "passive", value: "Passive Rewards" }, 
-    { key: "active", value:"Active Rewards" }
-  ]
+    { key: "passive", value: "Passive Rewards" },
+    { key: "active", value: "Active Rewards" },
+  ];
 
   const init = () => {
     const now = moment();
@@ -23,8 +23,13 @@
     console.log("curEpoch.id: ", curEpoch.id);
     console.log("epochs: ", epochs.default);
 
-    rows = epochs.default.filter((epoch) => epoch.id < curEpoch.id );
-    rows.sort((first, second) => {return second.id - first.id});
+    rows = epochs.default.filter((epoch) => epoch.id < curEpoch.id);
+    rows.forEach((row) => {
+      row.date_start = moment(row.date_start).format("DD-MM-YYYY");
+    });
+    rows.sort((first, second) => {
+      return second.id - first.id;
+    });
 
     console.log("rows: ", rows);
     loading = false;
@@ -36,8 +41,8 @@
 <div>
   <DataTable
     title="Data Farming History"
-    headers={headers}
-    rows={rows}
+    {headers}
+    {rows}
     class="customTable"
   />
 </div>
