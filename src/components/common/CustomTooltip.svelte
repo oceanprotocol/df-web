5cf4797b8bb87bed5badae0f84e550c1824e34bd
idiom-bytes <69865342+idiom-bytes@users.noreply.github.com>
2022-11-16 14:40:39 -0800

    Cleaned up tooltips, icons, colors, added multiple state for tooltip, adjusted all tooltip text and state so it's setup correctly.

--- src/components/common/CustomTooltip.svelte
+++ src/components/common/CustomTooltip.svelte
@@ -1,27 +1,42 @@
 <script>
   import { Tooltip } from "carbon-components-svelte";
-
+  import { InformationFilled } from "carbon-icons-svelte";
+  
   export let text = undefined;
   export let direction = "top";
   export let align = "center";
   export let open = false;
-  export let warning = undefined;
+  
+  // Use: "alert", "warning", or undefined
+  export let state = undefined; 
 </script>
 
 <div
   on:mouseleave={() => (open = false)}
   on:mouseenter={() => (open = true)}
-  class={`tooltipContiner ${warning ? "warning" : ""}`}
+  class={`tooltipContainer ${state === "alert" ? "alert" : state === "warning" ? "warning" : ''}`}
 >
-  <Tooltip {align} {direction} size={10} {open}>
-    {#if text}
-      <div class="textContainer">
-        <p class="text">
-          {@html text}
-        </p>
-      </div>
-    {/if}
-  </Tooltip>
+  {#if state === undefined}
+    <Tooltip {align} {direction} size={10} {open}>
+      {#if text}
+        <div class="textContainer">
+          <p class="text">
+            {@html text}
+          </p>
+        </div>
+      {/if}
+    </Tooltip>
+  {:else}
+    <Tooltip {align} {direction} size={10} {open} icon={InformationFilled}>
+      {#if text}
+        <div class="textContainer">
+          <p class="text">
+            {@html text}
+          </p>
+        </div>
+      {/if}
+    </Tooltip>
+  {/if}
 </div>
 
 <style>
@@ -37,6 +52,9 @@
     color: black;
     font-size: var(--font-size-mini);
     white-space: pre-wrap;
+  }
+  :global(.alert svg) {
+    fill: var(--brand-alert-red) !important;
   }
   :global(.warning svg) {
     fill: var(--brand-alert-yellow) !important;
