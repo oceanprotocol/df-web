63c571260732ab8217a6a0904096e999d8d7d8b5
Norbert <katunanorbert@gmail.com>
2022-12-16 16:53:50 +0200

    add support for different env

--- src/app.config.js
+++ src/app.config.js
@@ -16,16 +16,31 @@ export const getNetworkByChainId = async (chainId) => {
 };
 
 export const setupAppConfig = () => {
-    if(process.env.NODE_ENV == 'production'){
-        process.env.SUPPORTED_CHAIN_IDS = "[1]"
-        process.env.BACKEND_API = "https://df-sql.oceandao.org"
-        process.env.SUBGRAPH_API = "https://v4.subgraph.mainnet.oceanprotocol.com/subgraphs/name/oceanprotocol/ocean-subgraph"
-        process.env.VE_SUPPORTED_CHAINID = 1
-    }else{
-        process.env.SUPPORTED_CHAIN_IDS = "[5]"
-        process.env.BACKEND_API = "https://test-df-sql.oceandao.org"
-        process.env.SUBGRAPH_API = "https://v4.subgraph.goerli.oceanprotocol.com/subgraphs/name/oceanprotocol/ocean-subgraph"
-        process.env.VE_SUPPORTED_CHAINID = 5
-        process.env.DEBUGGING = "enabled"
+    switch(process.env.NODE_ENV){
+        case "production":
+            console.log("production")
+            process.env.SUPPORTED_CHAIN_IDS = "[1]"
+            process.env.BACKEND_API = "https://df-sql.oceandao.org"
+            process.env.SUBGRAPH_API = "https://v4.subgraph.mainnet.oceanprotocol.com/subgraphs/name/oceanprotocol/ocean-subgraph"
+            process.env.VE_SUPPORTED_CHAINID = 1
+            break
+        case "staging":
+            console.log("staging")
+            process.env.SUPPORTED_CHAIN_IDS = "[5]"
+            process.env.BACKEND_API = "https://test-df-sql.oceandao.org"
+            process.env.SUBGRAPH_API = "https://v4.subgraph.goerli.oceanprotocol.com/subgraphs/name/oceanprotocol/ocean-subgraph"
+            process.env.VE_SUPPORTED_CHAINID = 5
+            process.env.DEBUGGING = "enabled"
+            break
+        case "development":
+            console.log("development")
+            process.env.SUPPORTED_CHAIN_IDS = "[8996]"
+            process.env.BACKEND_API = "https://test-df-sql.oceandao.org"
+            process.env.SUBGRAPH_API = "https://v4.subgraph.goerli.oceanprotocol.com/subgraphs/name/oceanprotocol/ocean-subgraph"
+            process.env.VE_SUPPORTED_CHAINID = 8996
+            process.env.DEBUGGING = "enabled"
+            break
+        default:
+            console.log("NODE_ENV environment variable not set")
     }
 }
