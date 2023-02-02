c67e59751adc46aa55b351e159769e3ab3076d79
EnzoVezzaro <enzo-vezzaro@live.it>
2023-02-02 07:41:30 -0400

    Update app.config.js

--- src/app.config.js
+++ src/app.config.js
@@ -18,14 +18,14 @@ export const getNetworkByChainId = async (chainId) => {
 export const setupAppConfig = () => {
     switch(process.env.NODE_ENV){
         case "production":
-            console.log("production")
+            console.log("NODE_ENV has been set to production")
             process.env.SUPPORTED_CHAIN_IDS = "[1]"
             process.env.BACKEND_API = "https://df-sql.oceandao.org"
             process.env.SUBGRAPH_API = "https://v4.subgraph.mainnet.oceanprotocol.com/subgraphs/name/oceanprotocol/ocean-subgraph"
             process.env.VE_SUPPORTED_CHAINID = 1
             break
         case "staging":
-            console.log("staging")
+            console.log("NODE_ENV has been set to staging")
             process.env.SUPPORTED_CHAIN_IDS = "[5]"
             process.env.BACKEND_API = "https://test-df-sql.oceandao.org"
             process.env.SUBGRAPH_API = "https://v4.subgraph.goerli.oceanprotocol.com/subgraphs/name/oceanprotocol/ocean-subgraph"
@@ -33,7 +33,7 @@ export const setupAppConfig = () => {
             process.env.DEBUGGING = "enabled"
             break
         case "development":
-            console.log("development")
+            console.log("NODE_ENV has been set to development")
             process.env.SUPPORTED_CHAIN_IDS = "[8996]"
             process.env.BACKEND_API = "https://test-df-sql.oceandao.org"
             process.env.SUBGRAPH_API = "https://v4.subgraph.goerli.oceanprotocol.com/subgraphs/name/oceanprotocol/ocean-subgraph"
