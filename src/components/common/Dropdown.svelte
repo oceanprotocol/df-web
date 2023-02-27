7a3803ca54f290a81b4bca9931794e6542b233b8
Norbert <katunanorbert@gmail.com>
2023-02-27 13:54:02 +0200

    dropdown style improvements

--- src/components/common/Dropdown.svelte
+++ src/components/common/Dropdown.svelte
@@ -17,23 +17,14 @@
     :global(.bx--list-box__label){
         font-size: var(--font-size-base);
     }
-    :global(.bx--list-box__menu){
+    :global(.bx--list-box__menu), :global(.bx--list-box__field){
         background-color: white;
     }
-    :global(.bx--dropdown__wrapper){
-
-    }
-    :global(.bx--list-box__field){
-        background-color: white;
-    }
-    :global(.bx--list-box__label){
+    :global(.bx--list-box__label), :global(.bx--list-box__menu-item--active){
         font-weight: bold;
     }
     :global(.bx--dropdown){
         border: 0;
-    }
-    :global(.bx--list-box__menu-item--active){
-       font-weight: bold;
     }
     :global(.bx--list-box__menu-item--highlighted){
         background-color: var(--background-content) !important;
