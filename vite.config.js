5954c5fd1287b419e002244be31b7007f0c7a7cb
Norbert <katunanorbert@gmail.com>
2023-06-21 15:10:17 +0300

    removed rollup libraries

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
 import { defineConfig, loadEnv } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import commonjs from "@rollup/plugin-commonjs";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import nodePolyfills from "rollup-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
-    plugins: [
-      svelte(),
-      commonjs({
-        transformMixedEsModules: true,
-      }),
-      nodePolyfills(),
-    ],
+    plugins: [svelte()],
     server: {
       port: 3000, // Replace with the port number you want to use
       host: "localhost", // Replace with the host name you want to use
