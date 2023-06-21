0af10ee7e827086b4bbd2827b79c85447851c22f
Norbert <katunanorbert@gmail.com>
2023-06-21 17:07:08 +0300

    simplify Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return defineConfig({
-    plugins: [svelte()],
-    server: {
-      port: 3000, // Replace with the port number you want to use
-      host: "localhost", // Replace with the host name you want to use
-    },
-    define: {
-      "window.global": {},
-    },
-  });
-};
+export default defineConfig({
+  plugins: [svelte()],
+});
