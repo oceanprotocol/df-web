b97596387a353bbbfe1d47b83ca71eb021315f25
Norbert <katunanorbert@gmail.com>
2023-06-22 10:28:41 +0300

    use Vite preprocess

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), resolve(), commonjs()],
+  plugins: [svelte()],
 });
