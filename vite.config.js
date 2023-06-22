c6548769261322968426f0a3171992ac07464e1b
Norbert <katunanorbert@gmail.com>
2023-06-22 10:17:18 +0300

    added rollup plugins

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), resolve(), commonjs()],
 });
