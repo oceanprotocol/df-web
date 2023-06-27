00fa4c8abfaf120450426d80c07c6c674f15569a
Norbert <katunanorbert@gmail.com>
2023-06-27 11:39:50 +0300

    added base field to vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
+    base: "/",
     plugins: [
       svelte({
         preprocess: vitePreprocess(),
