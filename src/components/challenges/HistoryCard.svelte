13b537fdf5d10f0fba78884c2e0a074cbf0ec88c
Norbert <katunanorbert@gmail.com>
2023-06-30 12:08:43 +0300

    combine prize and winner column

--- src/components/challenges/HistoryCard.svelte
+++ src/components/challenges/HistoryCard.svelte
@@ -11,12 +11,9 @@
   let title = "Past Challenges";
   let headers = [
     { key: 'round', value: 'Round' },
-    { key: 'first_prize', value: '1st Prize' },
-    { key: 'first_place', value: 'Winner' },
-    { key: 'second_prize', value: '2nd Prize' },
-    { key: 'second_place', value: 'Winner' },
-    { key: 'third_prize', value: '3rd Prize' },
-    { key: 'third_place', value: 'Winner' }
+    { key: 'first_prize', value: '1st Prize Winner' },
+    { key: 'second_prize', value: '2nd Prize Winner' },
+    { key: 'third_prize', value: '3rd Prize Winner' },
   ];
 
   let rows = [];
@@ -30,12 +27,9 @@
       return {
         id: String(index + 1),
         round: row[0],
-        first_prize: parseInt(row[1]).toLocaleString(),
-        first_place: row[2],
-        second_prize: parseInt(row[3]).toLocaleString(),
-        second_place: row[4],
-        third_prize: parseInt(row[5]).toLocaleString(),
-        third_place: row[6]
+        first_prize: `${parseInt(row[1]).toLocaleString()} OCEAN - ${row[2].toLowerCase().substr(0, 6) + '...' + row[2].toLowerCase().substr(row[2].length - 6)}`,
+        second_prize: `${parseInt(row[3]).toLocaleString()} OCEAN - ${row[4].toLowerCase().substr(0, 6) + '...' + row[4].toLowerCase().substr(row[4].length - 6)}`,
+        third_prize: `${parseInt(row[5]).toLocaleString()} OCEAN - ${row[6].toLowerCase().substr(0, 6) + '...' + row[6].toLowerCase().substr(row[6].length - 6)}`,
       };
     });
 
@@ -85,14 +79,14 @@
             </svelte:fragment>
             <svelte:fragment slot="cell" let:cell let:row>
               <div class="cellContainer">
-                {#if cell.key === 'first_place' || cell.key === 'second_place' || cell.key === 'third_place'}
+                {#if cell.key === 'first_prize' || cell.key === 'second_prize' || cell.key === 'third_prize'}
                   <a
                     href={'https://etherscan.io/address/' + cell.value}
                     target="_blank"
                     rel="noreferrer"
                     class="link"
                   >
-                    {cell.value.toLowerCase().substr(0, 6) + '...' + cell.value.toLowerCase().substr(cell.value.length - 6)}
+                    {cell.value}
                     <img class="externalLink" src={ExternalIcon} alt="external link" />
                   </a>
                 {:else if cell.key === 'first_prize' || cell.key === 'second_prize' || cell.key === 'third_prize'}
