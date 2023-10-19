8f3202c3014da797ab7fd2f7ad93fcdcf5abd9b2
Norbert <katunanorbert@gmail.com>
2023-10-19 10:55:02 +0300

    updated terms text display to be aligned to left

--- src/components/footer/TermsOfUse.svelte
+++ src/components/footer/TermsOfUse.svelte
@@ -13,12 +13,19 @@
   <div class="container">
     <h2>Terms and Conditions</h2>
     <p class="description">Thanks for using our product and services</p>
-    <SvelteMarkdown source={markdownContent}/>
+    <div>
+        <SvelteMarkdown source={markdownContent}/>
+    </div>
   </div>
 
   <style>
     .container{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
         margin-top: calc(var(--spacer)*2);
+        text-align: left;
     }
     .description{
         margin-bottom: calc(var(--spacer));
