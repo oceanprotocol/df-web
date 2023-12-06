d00fb4b77a376e91490077152a853ebc7f070b39
Norbert <katunanorbert@gmail.com>
2023-12-06 12:17:31 +0200

    fix

--- src/components/common/Modal.svelte
+++ src/components/common/Modal.svelte
@@ -6,7 +6,7 @@
 
 	let dialog; // HTMLDialogElement
 
-	$: if (dialog) dialog.showModal();
+	$: if (dialog && showModal) dialog.showModal();
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
