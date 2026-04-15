8fead8788075bcaa8867feb126fcb2cbaa668163
graceful-coder <101472818+graceful-coder@users.noreply.github.com>
2026-04-15 21:58:24 +0000

    fixed OCEAN rewards typo to USDC

--- src/components/claim/RewardOverview.svelte
+++ src/components/claim/RewardOverview.svelte
@@ -10,7 +10,7 @@
     roundInfo?.streams?.forEach((stream) => {
       totalRewards+=parseInt(stream.rewards)
     })
-    return `${totalRewards.toLocaleString()} OCEAN`
+    return `${totalRewards.toLocaleString()} USDC`
   }
 </script>
 
